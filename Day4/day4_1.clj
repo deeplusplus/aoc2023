dbb7b2b5dfd4ca5bd8720d5dfb7c0e25ea1f5b50
CDROM <r2dan2d2@gmail.com>
2023-12-07 21:33:10 -0500

    Almost there....

--- Day4/day4_1.clj
+++ Day4/day4_1.clj
@@ -15,6 +15,17 @@
 (def no_card_line_list (map remove_card raw_line_list))
 
 (defn get_intersection [line]
-  (set/intersection (set (split_on_space (first (split_on_bar line)))) (set (split_on_space (last (split_on_bar line))))))
+  (disj (set/intersection (set (split_on_space (first (split_on_bar line)))) (set (split_on_space (last (split_on_bar line))))) ""))
 
-(println (map get_intersection no_card_line_list))
+(def seq_of_intersections (map get_intersection no_card_line_list))
+
+(def seq_of_counts (map count seq_of_intersections))
+
+(def one_point_count (count (filter (fn [set_count] (= 1 set_count)) seq_of_counts)))
+
+(def greater_than_two_counts (count (filter (fn [set_count] (> 1 set_count)) seq_of_counts)))
+
+(println seq_of_intersections)
+(println seq_of_counts)
+(println one_point_count)
+(println greater_than_two_counts)
