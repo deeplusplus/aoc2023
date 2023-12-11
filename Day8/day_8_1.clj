ed23151758446a4492da8c3eb13254820e651c5b
CDROM <r2dan2d2@gmail.com>
2023-12-10 20:15:45 -0500

    Attempting using modulo to eliminate need to pass entire collection of directions to recursion.

--- Day8/day_8_1.clj
+++ Day8/day_8_1.clj
@@ -2,7 +2,10 @@
 
 (def raw_line_list (str/split-lines (slurp "input.txt")))
 
-(def instructions (seq (str/join "" (take 2000 (cycle (first raw_line_list))))))
+(def instructions (seq (str/join "" (first raw_line_list))))
+
+(defn get_current_instruction[iteration]
+  (nth instructions (mod 281 iteration)))
 
 (def raw_nodes_list (nthrest raw_line_list 2))
 
@@ -13,10 +16,10 @@
 
 (def node_sequence (map make_node raw_nodes_list))
 
-(defn find_node [target_node all_nodes]
-  (first (filter (fn[node] (= target_node (:value node))) all_nodes)))
+(defn find_node [target_node]
+  (first (filter (fn[node] (= target_node (:value node))) node_sequence)))
 
-(defn find_next_node [current_node next_direction all_nodes all_directions iteration]
+(defn find_next_node [current_node next_direction all_directions iteration]
   (comment (println "AT NODE" (apply str [(:value current_node) ", L: " (:L current_node) ", R: " (:R current_node)])))
   (comment (println "GOING TO " next_direction))
   (comment (println iteration))
@@ -24,11 +27,11 @@
     (comp (println "FOUND ZZZ at iteration: " (str iteration)) (throw (Exception. "STOP")))
   )
   (if (= next_direction \L)
-    (find_next_node (find_node (:L current_node) all_nodes) (first all_directions) all_nodes (nthrest all_directions 1) (inc iteration)) 
-    (find_next_node (find_node (:R current_node) all_nodes) (first all_directions) all_nodes (nthrest all_directions 1) (inc iteration))
+    (find_next_node (find_node (:L current_node)) (first all_directions) (nthrest all_directions 1) (inc iteration)) 
+    (find_next_node (find_node (:R current_node)) (first all_directions) (nthrest all_directions 1) (inc iteration))
   )
 )
 
-(def starting_node (find_node "AAA" node_sequence))
+(def starting_node (find_node "AAA"))
 
-(println (find_next_node starting_node (first instructions) node_sequence (nthrest instructions 1) 0))
+(println (find_next_node starting_node (first instructions) (nthrest instructions 1) 0))
