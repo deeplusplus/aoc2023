fd6a3c75289c15f7eca2d003a43ace03305dcad0
CDROM <r2dan2d2@gmail.com>
2023-12-10 19:30:56 -0500

    Got it working on small sample.  Need to verify on larger sample and print count.

--- Day8/day_8_1.clj
+++ Day8/day_8_1.clj
@@ -2,7 +2,7 @@
 
 (def raw_line_list (str/split-lines (slurp "small_input1.txt")))
 
-(def instructions (seq (str/join "" (repeat 50 (first raw_line_list)))))
+(def instructions (seq (str/join "" (repeat 5 (first raw_line_list)))))
 
 (def raw_nodes_list (nthrest raw_line_list 2))
 
@@ -17,14 +17,21 @@
   (first (filter (fn[node] (= target_node (:value node))) all_nodes)))
 
 (defn find_next_node [current_node next_direction all_nodes all_directions]
-  (println "AT NODE" (str (:value current_node)))
-  (if (= (:value current_node) "ZZZ") (println "FOUND ZZZ")
-  (if (= next_direction "L")
-    (find_next_node (find_node (:L current_node) all_nodes) (first all_directions) all_nodes (drop 1 all_directions)) 
-    (find_next_node (find_node (:R current_node) all_nodes) (first all_directions) all_nodes (drop 1 all_directions))
-  )))
+  (println "AT NODE" (apply str [(:value current_node) ", L: " (:L current_node) ", R: " (:R current_node)]))
+  (println "GOING TO " next_direction)
+  (print (read-line))
+  (if (= (:value current_node) "ZZZ") 
+    (println "FOUND ZZZ!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    (println "DID NOT FIND ZZZ")
+  )
+  (if (= next_direction \L)
+    (find_next_node (find_node (:L current_node) all_nodes) (first all_directions) all_nodes (nthrest all_directions 1)) 
+    (find_next_node (find_node (:R current_node) all_nodes) (first all_directions) all_nodes (nthrest all_directions 1))
+  )
+)
 
 (def starting_node (find_node "AAA" node_sequence))
 
-(println (drop 1 instructions))
-(println (find_next_node starting_node (first instructions) node_sequence (drop 1 instructions)))
+(println starting_node)
+(println (type (first instructions)))
+(println (find_next_node starting_node (first instructions) node_sequence (nthrest instructions 1)))
