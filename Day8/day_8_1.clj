6444131db0b8c909158ef4841aeda5f5a525cd2f
CDROM <r2dan2d2@gmail.com>
2023-12-10 19:54:41 -0500

    Memory footprint too big.  Not yet sure how to approach without mutable state.

--- Day8/day_8_1.clj
+++ Day8/day_8_1.clj
@@ -1,8 +1,8 @@
 (require ['clojure.string :as 'str])
 
-(def raw_line_list (str/split-lines (slurp "small_input1.txt")))
+(def raw_line_list (str/split-lines (slurp "input.txt")))
 
-(def instructions (seq (str/join "" (repeat 5 (first raw_line_list)))))
+(def instructions (seq (str/join "" (take 2000 (cycle (first raw_line_list))))))
 
 (def raw_nodes_list (nthrest raw_line_list 2))
 
@@ -16,22 +16,19 @@
 (defn find_node [target_node all_nodes]
   (first (filter (fn[node] (= target_node (:value node))) all_nodes)))
 
-(defn find_next_node [current_node next_direction all_nodes all_directions]
-  (println "AT NODE" (apply str [(:value current_node) ", L: " (:L current_node) ", R: " (:R current_node)]))
-  (println "GOING TO " next_direction)
-  (print (read-line))
+(defn find_next_node [current_node next_direction all_nodes all_directions iteration]
+  (comment (println "AT NODE" (apply str [(:value current_node) ", L: " (:L current_node) ", R: " (:R current_node)])))
+  (comment (println "GOING TO " next_direction))
+  (comment (println iteration))
   (if (= (:value current_node) "ZZZ") 
-    (println "FOUND ZZZ!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    (println "DID NOT FIND ZZZ")
+    (comp (println "FOUND ZZZ at iteration: " (str iteration)) (throw (Exception. "STOP")))
   )
   (if (= next_direction \L)
-    (find_next_node (find_node (:L current_node) all_nodes) (first all_directions) all_nodes (nthrest all_directions 1)) 
-    (find_next_node (find_node (:R current_node) all_nodes) (first all_directions) all_nodes (nthrest all_directions 1))
+    (find_next_node (find_node (:L current_node) all_nodes) (first all_directions) all_nodes (nthrest all_directions 1) (inc iteration)) 
+    (find_next_node (find_node (:R current_node) all_nodes) (first all_directions) all_nodes (nthrest all_directions 1) (inc iteration))
   )
 )
 
 (def starting_node (find_node "AAA" node_sequence))
 
-(println starting_node)
-(println (type (first instructions)))
-(println (find_next_node starting_node (first instructions) node_sequence (nthrest instructions 1)))
+(println (find_next_node starting_node (first instructions) node_sequence (nthrest instructions 1) 0))
